533fdc5479b258bd81a291d00c8a353b803fef52
bllndalichako <bndalichako@ufl.edu>
2023-12-07 23:03:12 -0500

    Fixed "PROJECT" links

--- frontend/src/pages/team/Team.jsx
+++ frontend/src/pages/team/Team.jsx
@@ -1,7 +1,45 @@
+import Member from "../../components/member/Member";
+import "./Team.scss";
+import { singleProject } from "../../data";
+
 const Team = () => {
   return (
-    <div>Team</div>
-  )
-}
+    <div className="project">
+      <h1>Project Details</h1>
+      <div className="details">
+        <div className="item">
+          <span className="itemTitle">Name: </span>
+          <span className="itemValue">{singleProject.title}</span>
+        </div>
 
-export default Team
+        <div className="item">
+          <span className="itemTitle">Description: </span>
+          <span className="itemValue">{singleProject.description}</span>
+        </div>
+
+        <div className="item">
+          <span className="itemTitle">Technology Stack: </span>
+          <span className="itemValue">{singleProject.technologyStack}</span>
+        </div>
+
+        <div className="item">
+          <span className="itemTitle">Frameworks: </span>
+          <span className="itemValue">{singleProject.frameworks}</span>
+        </div>
+      </div>
+
+      <div className="personnel">
+        <h1>Project Personnel</h1>
+        <div className="personnelList">
+          {singleProject.members.map((member) => (
+            <div className="personnelListItem" key={member.id}>
+              <Member {...member} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Team;
